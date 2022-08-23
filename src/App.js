c6a6b58866bc84d9b02da7d2e5fc01d0e016da14
Jackalob <nike9712@gmail.com>
2022-08-23 11:52:00 +0800

    init

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import './App.css'
+import { HomePage } from './pages/Home'
+import { RQSuperHeroesPage } from './pages/RQSuperHeroes'
+import { SuperHeroesPage } from './pages/SuperHeroes'
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to='/'>Home</Link>
+            </li>
+            <li>
+              <Link to='/super-heroes'>Traditional Super Heroes</Link>
+            </li>
+            <li>
+              <Link to='/rq-super-heroes'>RQ Super Heroes</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path='/super-heroes'>
+            <SuperHeroesPage />
+          </Route>
+          <Route path='/rq-super-heroes'>
+            <RQSuperHeroesPage />
+          </Route>
+          <Route path='/'>
+            <HomePage />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  )
+}
+
+export default App
